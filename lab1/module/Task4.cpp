b8905a9e5034f5cec075fe376ed47e3b03468f66
ilKogan <vinsent.in7aider@gmail.com>
2020-04-24 14:57:49 +0300

    Again rewrite code task 4 and AGAIN need fix fucking error

--- lab1/module/Task4.cpp
+++ lab1/module/Task4.cpp
@@ -3,73 +3,240 @@
 #include <string>
 #include <time.h>
 
-#pragma warning(disable : 4996)
 
 using namespace std;
 
-#define NAME "Name"
-#define EXTENSION ".txt"
-#define SEPARATOR "/"
+#define SEPARATE '/'
+#define NUL "0"
+#define NEXTLINE '\n'
+#define DELETE_MARK -2
+#define SEPARATE_MARK -1
 
-void createManyFile(char* Name, int numberFiles, char* extension, string (*func)(int, int)) {
-	ofstream file;
-	char source[20];
-	for (int i = 0; i <= numberFiles; i++) {
-		char num[5];
-		itoa(i, num, 10);
-		strcpy(source, Name);
-		strcat(source, num);
-		strcat(source, extension);
-		file.open((char*)source, ios_base::out);
-		file << func(i, numberFiles);
-		file.close();
+enum MODE {
+	DAY,
+	MONTH,
+	YEAR
+};
+
+void createFile(string name, string content) {
+	ofstream oFile(name);
+	oFile << content;
+	oFile.close();
+}
+string readFile(string name) {
+	string temp;
+	string line;
+	ifstream iFile(name);
+	while (getline(iFile, line)) {
+		temp += line += "\n";
 	}
+	iFile.close();
+	return temp;
 }
-//FIX THIS
-char* randomDate(int yearBegin, int yearEnd) {
-	char date[15];
-	yearEnd -= yearBegin;
-
-	srand(time(0));
-	char year[5];
-	char month[3];
-	char day[3];
-	itoa(rand() % yearEnd + yearBegin, year, 10);
-	itoa(rand() % 12 + 1, month, 10);
-	itoa(rand() % 30 + 1, day, 10);
-	strcpy(date, day);
-	strcat(date, SEPARATOR);
-	strcat(date, month);
-	strcat(date, SEPARATOR);
-	strcat(date, year);
-	strcat(date, "\n");
-
-	return date;
+void writeFile(string name, string content) {
+	ofstream oFile;
+	oFile.open(name, ios_base::app | ios_base::ate);
+	oFile << content;
+	oFile.close();
 }
 
-string fillDataTime(int num, int max) {
-	string ret = "";
-	switch (num) {
-	case 0:
-		for (int i = 0; i < max; i++) {
-			ret = randomDate(1984, 2030);
+int countLineFile(string name) {
+	string tmp = readFile(name);
+	int ret = 0;
+	for (int i = 0; i < tmp.size(); i++) {
+		if (tmp[i] == NEXTLINE) {
+			ret++;
 		}
-		break;
-	default:
-		break;
 	}
 	return ret;
 }
 
+void get2DArrfromString(int** output, string source, int size1, int size2) {
+
+	for (int i = 0; i < size1; i++) {
+		for (int j = 0; j < size2; j++) {
+			if (source[j] != SEPARATE) {
+				//Fast convert char to int
+				output[i][j] = (source[j] - 48);
+			} else {
+				output[i][j] = SEPARATE_MARK;
+			}
+		}
+	}
+}
+void delete2DArr(int** arr, int size2) {
+	for (int i = 0; i < size2; i++) {
+		delete[] arr[i];
+	}
+	delete[] arr;
+}
+
+int** create2DArr(int size, int size2) {
+	int** ptrArr = new int* [size];
+	for (int i = 0; i < size; i++) {
+		ptrArr[i] = new int[size2];
+	}
+	return ptrArr;
+}
+
+void clearCertainElementDate(int** numDate,int size,int size2, MODE mode) {
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size2; j++) {
+			switch (mode) {
+			case DAY: 
+				if (j == 0 || j == 1) {
+					numDate[i][j] = DELETE_MARK;
+				}
+				break;
+			case MONTH:
+				if (j == 3 || j == 4) {
+					numDate[i][j] = DELETE_MARK;
+				}
+				break;
+			case YEAR:
+				if (j > 5 && j < 10) {
+					numDate[i][j] = DELETE_MARK;
+				}
+				break;
+			default:
+				break;
+			}
+		}
+	}
+}
+
+void getStringfrom2DArr(string* strDate, int** numDate,int size, int size2) {
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size2 - 1; j++) {
+			if (numDate[i][j] == DELETE_MARK) {
+				continue;
+			} else if(numDate[i][j] == SEPARATE_MARK) {
+				strDate[j] = SEPARATE;
+			} else {
+//TODO: FIX ERROR.
+				strDate[j] = numDate[i][j];
+			}
+		}
+	}
+}
+
+void deleteDateElement(string name, MODE mode) {
+	string tmpStrDate = readFile(name);
+	int countLines = countLineFile(name);
+	const int countCharLine = 10;
+	int** ptrNumDate = create2DArr(countLines, countCharLine);
+	string* ptrStrDate = &tmpStrDate;
+
+	get2DArrfromString(ptrNumDate, tmpStrDate, countLines, countCharLine);
+	clearCertainElementDate(ptrNumDate, countLines, countCharLine,mode);
+	getStringfrom2DArr(ptrStrDate, ptrNumDate, countLines, countCharLine);
+	createFile(name, tmpStrDate);
+	delete2DArr(ptrNumDate, countCharLine);
+	delete[] ptrStrDate;
+}
+
+
+void fillRandomDate(string name, int count, int yearBegin, int yearEnd) {
+	string date;
+	string year;
+	string month;
+	string day;
+
+	yearEnd -= yearBegin;
+	for (int i = 0; i < count; i++) {
+		srand(time(0) + i);
+		day = to_string(rand() % 30 + 1);
+		month = to_string(rand() % 12 + 1);
+		year = to_string(rand() % yearEnd + yearBegin);
+
+		if (day.size() == 1) {
+			day = (NUL + day);
+		}
+		if (month.size() == 1) {
+			month = (NUL + month);
+		}
+
+
+		date += day += SEPARATE;
+		date += month += SEPARATE;
+		date += year += NEXTLINE;
+
+		writeFile(name, date);
+		date.clear();
+	}
+}
+
+
 void Task4() {
-	char name[10] = NAME;
-	char extension[5] = EXTENSION;
 
-	string (*ptrFunc)(int, int);
-	ptrFunc = &fillDataTime;
+	createFile("Name.txt", "");
+	fillRandomDate("Name.txt", 10, 1945, 2001);
+	deleteDateElement("Name.txt",DAY);
 
-	createManyFile(name, 3, extension, ptrFunc);
+	//void(*ptrFunc)(MODE,int,ofstream, ifstream);
+	//ptrFunc = &fileDateManipulation;
+	//createAndFillFiles(name, 3, extension, ptrFunc);
 }
 
 
 
+
+//
+//void createAndFillFiles(char* Name, int numberFiles, char* extension, void(*func)(MODE, int,ofstream,ifstream)) {
+//	ofstream outputFile;
+//	ifstream inputFile;
+//	char source[20];
+//	for (int i = 0; i <= numberFiles; i++) {
+//		char num[5];
+//		itoa(i, num, 10);
+//		strcpy(source, Name);
+//		strcat(source, num);
+//		strcat(source, extension);
+//		outputFile.open((char*)source, ios_base::out);
+//		func(static_cast<MODE>(i), numberFiles, outputFile, inputFile);
+//		outputFile.close();
+//		inputFile.close();
+//	}
+//}
+////FIX THIS
+//string randomDate(int yearBegin, int yearEnd,string separator) {
+//	string date;
+//	yearEnd -= yearBegin;
+//
+//	srand(time(0));
+//	string year;
+//	string month;
+//	string day;
+//	year = to_string(rand() % yearEnd + yearBegin);
+//	month = to_string(rand() % 12 + 1);
+//	day = to_string(rand() % 30 + 1);
+//	date.append(day);
+//	date.append(separator);
+//	date.append(month);
+//	date.append(separator);
+//	date.append(year);
+//	date.append("\n");
+//
+//	return date;
+//}
+//
+//void fileDateManipulation(MODE mode, int max, ofstream fileOut,ifstream fileInput) {
+//	string temp = "";
+//	switch (mode) {
+//	case RANDOM_DATE:
+//		for (int i = 0; i < max; i++) {
+//			temp += randomDate(1984, 2030,SEPARATOR);
+//		}
+//		fileOut << temp;
+//		temp.clear();
+//		break;
+//	case DAY_MONTH:
+//		break;
+//	case DAY_YEAR:
+//		break;
+//	case MONTH_YEAR:
+//		break;
+//	default:
+//		break;
+//	}
+//}
