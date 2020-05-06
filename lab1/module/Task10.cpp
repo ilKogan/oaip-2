242e312481562f4585dea8d377202da9c3afbbcc
ilKogan <vinsent.in7aider@gmail.com>
2020-05-06 21:52:03 +0300

    Fix and complete all task

--- lab1/module/Task10.cpp
+++ lab1/module/Task10.cpp
@@ -1,8 +1,86 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+
+//10 
+//Скопировать из файла F1 в 
+//файл F2 все строки, которые не содержат цифры.
+//Подсчитать количество строк, которые начинаются 
+//на букву «А» в файле F2.
 
 using namespace std;
-void Task10() {
 
+
+void createFiles(string name, string content) {
+	ofstream oFile(name);
+	oFile << content;
+	oFile.close();
+}
+
+bool checkLineToNumber(string line) {
+	for (int i = 0; i < line.size(); i++) {
+		if (line[i] < 57 && line[i] > 49) {
+			return false;
+		}
+	}
+	return true;
+}
+
+void copyLineFileToFileCondition(string file1, string file2, bool(*func)(string)) {
+	ifstream in(file1);
+	ofstream out(file2);
+
+	string tmp;
+	string line;
+
+	while (getline(in, line)) {
+		if (func(line)) {
+			tmp += line;
+			tmp += "\n";
+		}
+	}
+
+	out << tmp;
+
+	in.close();
+	out.close();
+}
+
+int numbersLineBeginToChar(string fileName, char ch) {
+	ifstream in(fileName);
+	string line;
+	int countLine = 0;
+
+	while (getline(in, line)) {
+		if (line[0] == toupper(ch) || line[0] == tolower(ch)) {
+			countLine++;
+		}
+	}
+	return countLine;
+}
+
+
+void Task10() {
+	bool (*condition)(string);
+	condition = checkLineToNumber;
+
+	string str = "Somethink a23\n"
+		"Where; is my mind\n"
+		"Sector 4235-A;-235\n"
+		"Dead; end\n"
+		"alice what do you think?\n"
+		"apple\n"
+		"8-Universe ";
+
+	int countLineWithA;
+
+	createFiles("File1.txt", str);
+	createFiles("File2.txt", "");
+	copyLineFileToFileCondition("File1.txt", "File2.txt", condition);
+	countLineWithA = numbersLineBeginToChar("File2.txt", 'A');
+
+	cout << countLineWithA;
 
 }
 
--- lab1/module/Task12.cpp
+++ lab1/module/Task12.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
-void Task12() {
 
+//12 
+//ќпределить функцию, котора€, игнориру€ исходное деление файла на строки,
+//переформатирует его, разбива€ на строки так, чтобы кажда€ строка оканчивалась
+//Ђ; ї, или содержала 60 символов, если среди них нет Ђ; ї.
+
+void formatText(string fileName) {
+	ifstream in(fileName);
+
+	string tmpLine;
+	string tmpText;
+	string result;
+	int indexLastEndl = 0;
+
+	while(getline(in, tmpLine)){
+		tmpText += tmpLine;
+		tmpText += ' ';
+	}
+	in.close();
+
+	ofstream out(fileName);
+
+	for (int i = 0; i < tmpText.size(); i++) {
+		if (tmpText[i] == ';') {
+			indexLastEndl = i;
+			result += tmpText[i];
+			result += "\n";
+		} else if (i == (indexLastEndl + 60)) {
+			indexLastEndl = i;
+			result += tmpText[i];
+			result += "\n";
+		} else {
+			result += tmpText[i];
+		}
+	}
+	out << result;
+}
+
+void Task12() {
+	formatText("TextFormat.txt");
 }
 
