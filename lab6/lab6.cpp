c05f0206b3f6362ff6c3749f57b275cc9f733552
ilKogan <vinsent.in7aider@gmail.com>
2020-09-02 13:48:00 +0300

    Add project files to lab 6

---
+++ lab6/lab6.cpp
@@ -0,0 +1,20 @@
+#include "Lab6.h"
+
+int main() {
+
+	while (true) {
+		int choice = 0;
+		std::cin >> choice;
+
+		switch (choice) {
+		case 0: std::cin >> choice;
+			break;
+		case 3: Task3();
+			break;
+		case 4: Task4();
+			break;
+		default: std::system("pause");
+			break;
+		}
+	}
+}
---
+++ lab6/lab6.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <iostream>
+
+void Task3();
+void Task4();
