bbfc6d1103233c369310692e1ffea1425c04fc7c
ilKogan <vinsent.in7aider@gmail.com>
2020-09-04 18:26:41 +0300

    test telegram notifications

--- lab6/module/Task3.cpp
+++ lab6/module/Task3.cpp
@@ -1,10 +1,68 @@
-#include <iostream>
-
-using namespace std;
+//#include <iostream>
+//
+//using namespace std;
+//
+//template<typename T>
+//class Node {
+//
+//	public:
+//		Node* pNext;
+//		T data;
+//		Node(T data = T(), Node* pNext = nullptr) {
+//		this->data = data;
+//		this->pNext = pNext;
+//	}
+//};
+//
+//
+//template<typename T>
+//class List {
+//	public:
+//		List();
+//		~List();
+//		int getSize() { return size; }
+//		void push_back(T data);
+//
+//	private:
+//		int size;
+//		Node<T> *head;
+//
+//};
+//
+//template<typename T>
+//List<T>::List() {
+//	size = 0;
+//	head = nullptr;
+//};
+//
+//template<typename T>
+//List<T>::~List() {
+//}
+//
+//template<typename T>
+//void List<T>::push_back(T data) {
+//	if (head == nullptr) {
+//		head = new Node<T>(data);
+//	} else {
+//		Node<T>* current = this->head;
+//			while (current->pNext != nullptr) {
+//				current = current->pNext;
+//			}
+//			current->pNext = new Node<T>(data);
+//	}
+//	size++;
+//};
+//
+//
 void Task3() {
-
-
+//	List<int> lst;
+//	lst.push_back(5);
+//	lst.push_back(10);
+//	lst.push_back(12);
+//
+//	
+//
 }
-
-
-
+//
+//
+//
--- lab6/module/Task4.cpp
+++ lab6/module/Task4.cpp
@@ -1,7 +1,63 @@
 #include <iostream>
 
 using namespace std;
-void Task4() {
 
+#define EMPTY nullptr
+
+template<typename T>
+class Node {
+public:
+	Node *ptrNextNode;
+	T data;
+	Node(T data = T(), Node* ptrNextNode = EMPTY) {
+		this->data = data;
+		this->ptrNextNode = ptrNextNode;
+	}
+};
+
+template<typename T>
+class List {
+public:
+	int getsize() {
+		return size;
+	}
+	void push_back(T data){
+		if (ptrHeadNode == nullptr) {
+			ptrHeadNode = new Node<T>(data);
+		} else {
+			Node<T>* ptrCurrentNode = this->ptrHeadNode;
+
+			while (ptrCurrentNode->ptrNextNode != nullptr) {
+				ptrCurrentNode = ptrCurrentNode->ptrNextNode;
+			}
+			ptrCurrentNode->ptrNextNode = new Node<T>(data);
+		}
+		size++;
+	};
+	void push_front(T data){
+		Node<T>* ptrPreviosNode = new Node<T>(data,ptrHeadNode);
+		ptrHeadNode = ptrPreviosNode;
+		size++;
+
+	};
+	void pop_back(){};
+	void pop_front(){};
+	void insert(int position,T data);
+	void unique();
+	void merge(List<T> spisok);
+	T front();
+	T back();
+
+private:
+	Node<T> *ptrHeadNode;
+	int size;
+};
+
+
+void Task4() {
+	List<int> lst;
+	lst.push_back(1);
+	lst.push_back(2);
+	lst.push_front(3);
 }
 
